b1ed8e0907633aacced95f1bcc805e40ff1f133d
Lamberk <komarovvv36@gmail.com>
2016-05-25 19:33:11 +0300

    New grid for 290x350 output

--- app/helpers.py
+++ app/helpers.py
@@ -49,18 +49,22 @@ def generate_action_item():
     action_with_border = ImageOps.expand(action_img, border=2, fill='black')
     return action_with_border
    
-def generate_image(image_url, price, product_title, free_shipping, loyality_programm, pricespecial=0, output_size=(340,340)):
-    
+def generate_image(image_url, price, product_title, free_shipping, loyality_programm, output_size, pricespecial=0):
+    print output_size
+    if not output_size:
+        output_size = (340, 340)
+
     price_to_print = '{}$'.format(price)
     new_price_to_print = '{}$'.format(pricespecial)
     
     img = load_image(image_url)
-    resize_image(img, output_size)
+    image_size = (int(0.6*output_size[0]), int(0.6*output_size[0]))
+    resize_image(img, image_size)
     img_w, img_h = img.size
     
-    background = Image.new('RGBA', (int(1.2*img_w), int(1.6*img_h)), (255, 255, 255, 255))
-    bg_w, bg_h = background.size
-    offset = (int(0.1*bg_w), int(0.05*bg_h))
+    background = Image.new('RGBA', output_size, (255, 255, 255, 255))
+    bg_w, bg_h = output_size
+    offset = (int((bg_w-img_w)/2), int(0.05*bg_h))
     
     background.paste(img, offset)
 
@@ -69,25 +73,21 @@ def generate_image(image_url, price, product_title, free_shipping, loyality_prog
     large_font = get_font(22)
     plus_font = get_font(22, bold=True)
 
-    title_offset = (int(0.1*img_w), int(1.2*img_h))
+    title_offset = (int(0.1*bg_w), int(1.2*img_h))
     previous_price_offset = (title_offset[0], title_offset[1] + 20)
-    new_price_offset = (title_offset[0], previous_price_offset[1] + 20)
+    new_price_offset = (title_offset[0], previous_price_offset[1] + 30)
     
-    new_title = cut_title(product_title, small_font, output_size[0])
+    new_title = cut_title(product_title, small_font, int(0.85*output_size[0]))
     draw.text(title_offset, new_title, 'black', font=small_font)
 
     if pricespecial > 0:
-        previous_price_offset = (title_offset[0], title_offset[1] + 20)
-        new_price_offset = (title_offset[0], previous_price_offset[1] + 20)
         draw.text(previous_price_offset, strike(str(price_to_print)), 'grey', font=small_font)
         draw.text(new_price_offset, str(new_price_to_print), 'red', font=large_font)
     else:
-        new_price_offset = (title_offset[0], title_offset[1] + 20)
         draw.text(new_price_offset, str(price_to_print), 'red', font=large_font)
 
-    free_shipping_offset = (title_offset[0] + img_w - 120, title_offset[1] + 50)
-    loyality_offset = (title_offset[0] + img_w - 35, title_offset[1] + 20)
-    mix_offset = (free_shipping_offset[0], loyality_offset[1])
+    free_shipping_offset = (0.9*bg_w-large_font.getsize('Envío gratis')[0], new_price_offset[1])
+    loyality_offset = (0.9*bg_w-large_font.getsize('plus')[0], free_shipping_offset[1]-30)
 
     if loyality_programm and free_shipping:
         draw.text(free_shipping_offset, 'Envío gratis', 'green', font=large_font)
@@ -95,7 +95,7 @@ def generate_image(image_url, price, product_title, free_shipping, loyality_prog
     elif loyality_programm:
         draw.text(loyality_offset, 'plus', 'orange', font=large_font)
     elif free_shipping:
-        draw.text(mix_offset, 'Envío gratis', 'green', font=large_font)
+        draw.text(free_shipping_offset, 'Envío gratis', 'green', font=large_font)
     action_item = generate_action_item()
     background.paste(action_item, (title_offset[0], new_price_offset[1] + 40))
     return background
